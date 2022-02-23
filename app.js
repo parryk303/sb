a741f5b864a4f80df1242204cea145940486c20c
parryk303 <kyle.parry.303@gmail.com>
2022-02-23 11:29:43 -0700

    Add update slash command

--- app.js
+++ app.js
@@ -15,15 +15,15 @@ const app = new App({
   appToken: process.env.APP_TOKEN
 });
 
-// app.command("/knowledge", async ({ command, ack, say }) => {
-//     try {
-//       await ack();
-//       say("Yaaay! that command works!");
-//     } catch (error) {
-//         console.log("err")
-//       console.error(error);
-//     }
-// });
+app.command("/knowledge", async ({ command, ack, say }) => {
+    try {
+      await ack();
+      say("Yaaay! that command works!");
+    } catch (error) {
+        console.log("err")
+      console.error(error);
+    }
+});
 
 app.command("/knowledge", async ({ command, ack, say }) => {
     try {
@@ -77,6 +77,76 @@ app.message(/hey/, async ({ command, say }) => {
       console.error(error);
     }
 });
+
+app.message(/products/, async ({ command, say }) => {
+    try {
+      let message = { blocks: [] };
+      const productsFAQs = faqs.data.filter((faq) => faq.keyword === "products");
+  
+      productsFAQs.map((faq) => {
+        message.blocks.push(
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "*Question ❓*",
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: faq.question,
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "*Answer ✔️*",
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: faq.answer,
+            },
+          }
+        );
+      });
+  
+      say(message);
+    } catch (error) {
+      console.log("err");
+      console.error(error);
+    }
+  });
+
+  app.command("/update", async ({ command, ack, say }) => {
+    try {
+      await ack();
+      const data = command.text.split("|");
+      const newFAQ = {
+        keyword: data[0].trim(),
+        question: data[1].trim(),
+        answer: data[2].trim(),
+      };
+      // save data to db.json
+      fs.readFile("db.json", function (err, data) {
+        const json = JSON.parse(data);
+        json.data.push(newFAQ);
+        fs.writeFile("db.json", JSON.stringify(json), function (err) {
+          if (err) throw err;
+          console.log("Successfully saved to db.json!");
+        });
+      });
+      say(`You've added a new FAQ with the keyword *${newFAQ.keyword}.*`);
+    } catch (error) {
+      console.log("err");
+      console.error(error);
+    }
+  });
 
 (async () => {
   const port = 3000
