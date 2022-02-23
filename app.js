7075547432d09465b0d73b325c3e2a3f51eb6d0a
parryk303 <kyle.parry.303@gmail.com>
2022-02-23 11:13:35 -0700

    Add db.json and .gitignore

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
+// require the fs module that's built into Node.js
+const fs = require('fs')
+// get the raw data from the db.json file
+let raw = fs.readFileSync('db.json');
+// parse the raw bytes from the file as JSON
+let faqs= JSON.parse(raw)
+
 const { App } = require("@slack/bolt");
 require("dotenv").config();
 // Initializes your app with your bot token and signing secret
@@ -8,15 +15,58 @@ const app = new App({
   appToken: process.env.APP_TOKEN
 });
 
+// app.command("/knowledge", async ({ command, ack, say }) => {
+//     try {
+//       await ack();
+//       say("Yaaay! that command works!");
+//     } catch (error) {
+//         console.log("err")
+//       console.error(error);
+//     }
+// });
+
 app.command("/knowledge", async ({ command, ack, say }) => {
     try {
       await ack();
-      say("Yaaay! that command works!");
+      let message = { blocks: [] };
+      faqs.data.map((faq) => {
+        message.blocks.push(
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: "*Question*",
+            },
+          },
+          {
+            type: "section",
+            text: {
+              type: "mrkdwn",
+              text: faq.question,
+            },
+          },
+          {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: "*Answer*",
+              },
+            },
+            {
+              type: "section",
+              text: {
+                type: "mrkdwn",
+                text: faq.answer,
+              },
+            }
+        );
+      });
+      say(message);
     } catch (error) {
-        console.log("err")
+      console.log("err");
       console.error(error);
     }
-});
+  });
 
 // matches any string that contains the string hey
 app.message(/hey/, async ({ command, say }) => {
