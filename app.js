1d8c27f1d42fc229c14815d561d1a23dbffd3477
parryk303 <kyle.parry.303@gmail.com>
2022-02-23 11:04:02 -0700

    Add app.js, knowledge slash command

---
+++ app.js
@@ -0,0 +1,36 @@
+const { App } = require("@slack/bolt");
+require("dotenv").config();
+// Initializes your app with your bot token and signing secret
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+  socketMode:true, // enable the following to use socket mode
+  appToken: process.env.APP_TOKEN
+});
+
+app.command("/knowledge", async ({ command, ack, say }) => {
+    try {
+      await ack();
+      say("Yaaay! that command works!");
+    } catch (error) {
+        console.log("err")
+      console.error(error);
+    }
+});
+
+// matches any string that contains the string hey
+app.message(/hey/, async ({ command, say }) => {
+    try {
+      say("Yaaay! that command works!");
+    } catch (error) {
+        console.log("err")
+      console.error(error);
+    }
+});
+
+(async () => {
+  const port = 3000
+  // Start your app
+  await app.start(process.env.PORT || port);
+  console.log(`⚡️ Slack Bolt app is running on port ${port}!`);
+})();
